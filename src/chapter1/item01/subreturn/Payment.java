e5315eea4fea40365864b2123ea8cd3c1edc7519
shin.jewoo <shinjw0926@naver.com>
2023-04-23 14:24:19 +0900

    item01. 장점5 정적 팩터리 메서드를 작성하는 시점에는 반환할 객체의 클래스가 존재하지 않아도 된다.

--- src/chapter1/item01/subreturn/Payment.java
+++ src/chapter1/item01/subreturn/Payment.java
@@ -1,5 +1,7 @@
 package chapter1.item01.subreturn;
 
+import java.lang.reflect.Constructor;
+
 public interface Payment {
 
     void pay();
@@ -9,6 +11,15 @@ public interface Payment {
             return new SamsungPay();
         } else if ("iphone".equals(phone)) {
             return new ApplePay();
+        } else if ("kakaopay".equals(phone)) {
+            Payment kakaoPay = null;
+            try {
+                Constructor<?> kakaoPayConstructor = Class.forName("org.kakao.sample.KakaoPay").getConstructor();
+                kakaoPay = (Payment) kakaoPayConstructor.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return kakaoPay;
         } else {
             throw new IllegalStateException("올바르지 않은 기종입니다.");
         }

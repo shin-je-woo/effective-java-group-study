38ecbfff6d89d7c5aff405c60aed0f0a685e9e15
shin.jewoo <shinjw0926@naver.com>
2023-05-28 19:37:52 +0900

    [item48] 예제코드

---
+++ src/chapter7/item48/ParallelMersennePrimes.java
@@ -0,0 +1,22 @@
+package chapter7.item48;
+
+import java.math.BigInteger;
+import java.util.stream.Stream;
+
+import static java.math.BigInteger.ONE;
+import static java.math.BigInteger.TWO;
+
+public class ParallelMersennePrimes {
+
+    public static void main(String[] args) {
+        primes().map(p -> TWO.pow(p.intValueExact()).subtract(ONE))
+                .parallel() // 스트림 병렬화
+                .filter(mersenne -> mersenne.isProbablePrime(50))
+                .limit(20)
+                .forEach(System.out::println);
+    }
+
+    static Stream<BigInteger> primes() {
+        return Stream.iterate(TWO, BigInteger::nextProbablePrime);
+    }
+}
